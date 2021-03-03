42c41fbe44131935418e075e217526694724d2d0
Paula63 <x17370023@student.ncirl.ie>
2021-03-03 23:16:55 +0000

    adding loading state for sign-up

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,32 @@
-import React, {useRef} from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import React, {useRef, useState} from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup(){
 
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const { signup, currentUser } = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        if(passwordRef.current.value !== passwordConfirmRef.current.value) {
+            return setError('Passwords do not match')
+        }
+
+        try {
+            setError('')
+            setLoading(true)
+            await signup(emailRef.current.value, passwordRef.current.value)
+        } catch {
+            setError('Failed to create an account')
+        }
+        setLoading(false)
+    }
 
     return(
         <>
@@ -14,7 +35,9 @@ export default function Signup(){
                     <h2 className = "text-center mb-4">
                         Sign up
                     </h2>
-                    <Form>
+                    {currentUser.email}
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required />
@@ -27,7 +50,7 @@ export default function Signup(){
                             <Form.Label>Password Confirm</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
-                        <Button className="w-100" type="Submit">Sign up</Button>
+                        <Button disabled={loading} className="w-100" type="Submit">Sign up</Button>
                     </Form>
                 </Card.Body>
             </Card>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,15 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { auth } from '../firebase';
+import React, { useContext, useEffect, useState } from 'react'
+import { auth } from '../firebase'
 
 const AuthContext =  React.createContext();
 
 export function useAuth() {
-    return useContext(AuthContext);
+    return useContext(AuthContext)
 }
 
 export function AuthProvider({ children }) {
 
     const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true);
 
     function signup(email, password) {
         return auth.createUserWithEmailAndPassword(email, password);
@@ -17,20 +18,20 @@ export function AuthProvider({ children }) {
 
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
-            setCurrentUser(user);
+            setCurrentUser(user)
+            setLoading(false)
         })
-        return unsubscribe;
+        return unsubscribe
     }, []);
 
     const value = {
         currentUser,
         signup
     }
-    //20:20
 
     return (
         <AuthContext.Provider value={value}>
-            {children}
+            {!loading && children}
         </AuthContext.Provider>
     )
 }

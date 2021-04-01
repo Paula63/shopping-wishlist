81c22b305e925d5e8eb2589829e80bc5b925fa7a
Paula63 <x17370023@student.ncirl.ie>
2021-04-01 22:15:08 +0100

    creating sidebar for home page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import AppBar from './app-bar/AppBar'
+import UserSidebar from './user-sidebar/UserSidebar'
 
 export default function Dashboard() {
     return (
         <>
             <AppBar />
+            <UserSidebar />
         </>
     )
 }

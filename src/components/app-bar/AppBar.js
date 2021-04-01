5989105b10681e1d63fa7d4493aa801b6dcf29ca
Paula63 <x17370023@student.ncirl.ie>
2021-04-01 15:31:04 +0100

    creating reusable navbar

---
+++ src/components/app-bar/AppBar.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Alert } from 'react-bootstrap'
+import { useAuth } from './../../contexts/AuthContext';
+import { DashboardContainer, BarWrap, AppLogo, ButtonWrap, ButtonLink } from './AppBarStyles';
+
+
+export default function AppBar() {
+
+    const [error, setError] = useState('')
+    const { logout } = useAuth()
+    const history = useHistory()
+
+    async function handleLogout() {
+        setError('')
+
+        try{
+            await logout()
+            history.push('/login')
+        } catch{
+            setError('Failed to log out')
+        }
+    }
+
+    return (
+        <>
+            <DashboardContainer>
+                <BarWrap>
+                    <AppLogo to="/">Shopping Wishlist</AppLogo>
+                    <ButtonWrap>
+                        {error && <Alert variant="danger">{error}</Alert>}
+                        <ButtonLink onClick={handleLogout}>Sign Out</ButtonLink>
+                    </ButtonWrap>
+                </BarWrap>
+            </DashboardContainer>
+        </>
+    )
+}

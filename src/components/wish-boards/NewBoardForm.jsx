1b9822e33fdfca125c38899ea242f754ffd7603c
Paula63 <x17370023@student.ncirl.ie>
2021-04-23 22:01:33 +0100

    adding navbar component

--- src/components/wish-boards/NewBoardForm.jsx
+++ src/components/wish-boards/NewBoardForm.jsx
@@ -3,6 +3,7 @@ import { Container, Card, Form } from 'react-bootstrap';
 import { Button } from '../ButtonElement';
 import { Link } from 'react-router-dom';
 import cuid from 'cuid';
+import AppBar from '../app-bar/AppBar';
 
 export default function NewBoardForm({ setBoard, createBoard }) {
 
@@ -22,42 +23,45 @@ export default function NewBoardForm({ setBoard, createBoard }) {
     }
 
     return (
-        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-            <div className="w-100" style={{maxWidth: '400px'}}>
-                <Card>
-                    <Card.Body>
-                        <h2 className="text-center mb-4">
-                            Create New Wish Board
-                        </h2>
-                        <Form onSubmit={handleFormSubmit}>
-                            <Form.Group>
-                                <Form.Label>Wish Board Name</Form.Label>
-                                <Form.Control 
-                                    type="text" 
-                                    placeholder="Enter Wish Board Name" 
-                                    name="title"
-                                    value={values.title} 
-                                    onChange={(e) => handleInputChange(e)} 
-                                    required 
-                                />
-                            </Form.Group>
-                            <Button 
-                                to="/boards" 
-                                primary="true" 
-                                dark="true"
-                                exact='true' 
-                                offset={-80}
-                                style={{ height: '40px' }}
-                            >
-                                Save
-                            </Button>
-                        </Form>
-                    </Card.Body>
-                </Card>
-                <div className = "w-100 text-center mt-2">
-                    <Link to="/boards">Cancel</Link>
+        <>
+            <AppBar />
+            <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh", marginTop: '-80px' }}>
+                <div className="w-100" style={{maxWidth: '400px'}}>
+                    <Card>
+                        <Card.Body>
+                            <h2 className="text-center mb-4">
+                                Create New Wish Board
+                            </h2>
+                            <Form onSubmit={handleFormSubmit}>
+                                <Form.Group>
+                                    <Form.Label>Wish Board Name</Form.Label>
+                                    <Form.Control 
+                                        type="text" 
+                                        placeholder="Enter Wish Board Name" 
+                                        name="title"
+                                        value={values.title} 
+                                        onChange={(e) => handleInputChange(e)} 
+                                        required 
+                                    />
+                                </Form.Group>
+                                <Button 
+                                    to="/boards" 
+                                    primary="true" 
+                                    dark="true"
+                                    exact='true' 
+                                    offset={-80}
+                                    style={{ height: '40px' }}
+                                >
+                                    Save
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                    <div className = "w-100 text-center mt-2">
+                        <Link to="/boards">Cancel</Link>
+                    </div>
                 </div>
-            </div>
-        </Container>
+            </Container>
+        </>
     )
 }

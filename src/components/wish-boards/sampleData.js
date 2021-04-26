46f64adf77c53742a6feadda53a0f52539bb9f97
Paula63 <x17370023@student.ncirl.ie>
2021-04-26 10:47:44 +0100

    using mock data for testing

---
+++ src/components/wish-boards/sampleData.js
@@ -0,0 +1,14 @@
+export const sampleData = [
+    {
+        id: '1',
+        title: 'Furniture'
+    },
+    {
+        id: '2',
+        title: 'Fashion'
+    },
+    {
+        id: '3',
+        title: 'Beauty'
+    }
+]

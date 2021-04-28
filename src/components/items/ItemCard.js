9f5e0fda3890651b7e0f3a72ff49a14216533d83
Paula63 <x17370023@student.ncirl.ie>
2021-04-28 23:54:01 +0100

    updating layout

--- src/components/items/ItemCard.js
+++ src/components/items/ItemCard.js
@@ -3,16 +3,16 @@ import './container.css';
 
 const ItemCard = props => {
     return (
-        <div className="holder text-center shadow" style={{ width: '240px', height: '420px' }}>
-            <div className="overflow">
-                <a href="fp">
-                    <img src={props.imgsrc} alt="item" className="holder-img-top" />
+        <div className="item text-center shadow" style={{ width: '240px', height: '420px' }}>
+            <div className="cont cont-size">
+                <a href={props.link}>
+                    <img src={props.imgsrc} alt="item" className="item-img-top" />
                 </a>
             </div>
-            <div className="holder-body text-dark">
-                <p className="holder-title" style={{ marginTop: '-40px' }}>{props.title}</p>
-                <p className="holder-text text-secondary">{props.desc}</p>
-                <p className="holder-text text-secondary">{props.price}</p>
+            <div className="item-title text-dark">
+                <p className="item-name" style={{ marginTop: '-40px' }}>{props.name}</p>
+                <p className="item-text text-secondary">Size: {props.size}</p>
+                <p className="item-text text-secondary">Price: {props.price}</p>
             </div>
         </div>
     )
--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -13,13 +13,26 @@ export default function Items() {
             <div className="container-fluid d-flex justify-content-center">
                 <div className="row">
                     <div className="col-md-4">
-                        <ItemCard imgsrc={bag} title="Versace Bag" desc="La Medusa Studded Round Camera Bag" link='https://www.amazon.com/' price="€1150.00" />
+                        <ItemCard imgsrc={bag} name="La Medusa Studded Round Camera Bag" size="Small" link='https://www.amazon.com/' price="€1150.00" />
                     </div>
                     <div className="col-md-4">
-                        <ItemCard imgsrc={vans} title="The Old Skool Vans" desc="Classic side stripe skate shoe with sturdy canvas." link='https://www.amazon.com/' price="€85.00 " />
+                        <ItemCard imgsrc={vans} name="Classic side stripe skate shoe" size="6" link='https://www.amazon.com/' price="€85.00 " />
                     </div>
                     <div className="col-md-4">
-                        <ItemCard imgsrc={coat} title="Bulbery Trench Coat" desc="Vintage Check Panel Cotton Gabardine Coat" link='https://www.amazon.com/' price="$2,850.00" />
+                        <ItemCard imgsrc={coat} name="Vintage Check Panel Cotton Gabardine Coat" size="Medium" link='https://www.amazon.com/' price="$2,850.00" />
+                    </div>
+                </div>
+            </div>
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="row">
+                    <div className="col-md-4">
+                        <ItemCard imgsrc={bag} name="La Medusa Studded Round Camera Bag" size="Small" link='https://www.amazon.com/' price="€1150.00" />
+                    </div>
+                    <div className="col-md-4">
+                        <ItemCard imgsrc={vans} name="Classic side stripe skate shoe" size="6" link='https://www.amazon.com/' price="€85.00 " />
+                    </div>
+                    <div className="col-md-4">
+                        <ItemCard imgsrc={coat} name="Vintage Check Panel Cotton Gabardine Coat" size="Medium" link='https://www.amazon.com/' price="$2,850.00" />
                     </div>
                 </div>
             </div>

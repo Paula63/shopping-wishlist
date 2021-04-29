1728570cf2f997cef8d85a086776df2db89c4739
Paula63 <x17370023@student.ncirl.ie>
2021-04-29 12:05:33 +0100

    removing dependency

--- src/components/wish-boards/Wishboards.jsx
+++ src/components/wish-boards/Wishboards.jsx
@@ -24,6 +24,7 @@ export default function Wishboards() {
 
     useEffect(() => {
         getWishboards();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     if (loading) {

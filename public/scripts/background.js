c613b0caced3930a49e4f3d8697b640e4b80e7d2
Paula63 <x17370023@student.ncirl.ie>
2021-05-02 21:00:35 +0100

    changing browser actions

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,12 +1,17 @@
-console.log("Lets go!");
-
 // browser action is a chrome API there is also a messaging api for allowing 
 // the ackgorund script to communicate with the content script
-chrome.browserAction.onClicked.addListener(clickExtension);
+// chrome.browserAction.onClicked.addListener(clickExtension);
 
-function clickExtension(tab){
-    let msg = {
-        txt: "hello"
-    }
-    chrome.tabs.sendMessage(tab.id, msg);
-}
+// function clickExtension(tab){
+//     let msg = {
+//         txt: "hello"
+//     }
+//     chrome.tabs.sendMessage(tab.id, msg);
+// }
+
+chrome.webRequest.onBeforeRequest.addListener(
+    function(datails) {
+        return {cancel: false}
+    },
+    {urls: ["<all_urls>"]}
+)

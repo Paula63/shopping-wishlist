b91527742426963d181a01654696f5a58d408dc6
Paula63 <x17370023@student.ncirl.ie>
2021-05-02 17:50:08 +0100

    getting the selected option from dropdown list and radio option

--- public/scripts/popup.js
+++ public/scripts/popup.js
@@ -1,14 +1,38 @@
-document.querySelector('input[name="reduction"]:checked').value();
+// getting the selected radio button from the list of options
+// document.querySelector('input[name="reduction"]:checked').value;
 
 // if above does not work
-// var radioBtn = document.getElementsByName('reduction');
-// var radio_value;
-// for(var i=0; i<radioBtn.length; i++) {
-//     if(radioBtn[i].checked){
-//         radio_value = radioBtn[i].value;
+var radioBtn = document.getElementsByName('reduction');
+var radio_value;
+for(var i=0; i<radioBtn.length; i++) {
+    if(radioBtn[i].checked){
+        radio_value = radioBtn[i].value;
+    }
+}
+
+// getting the selected object
+// var dropSelect = document.getElementsByName('list');
+// var dropList = document.getElementsByName('list');
+// var optionValue;
+
+// function setOption(dropList, optionValue) {
+//     for(var j=0; j<dropList.options.length; j++) {
+//         if(dropList.options[j].text === optionValue) {
+//             dropList.options[j].selected = true;
+//         }
 //     }
 // }
 
+// getting the selected object
+var dropSelect = document.getElementsByName('list');
+var optionValue = dropSelect.value;
+// for(var j=0; j<radioBtn.length; j++) {
+//     if(dropSelect[j].selected){
+//         optionValue = radioBtn[j].value;
+//     }
+// }
+
+// eslint-disable-next-line no-undef
 chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
 
     //check the current tab

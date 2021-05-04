4fa54ccbf68b3f5e0f1d42feb66309f31784e346
Paula63 <x17370023@student.ncirl.ie>
2021-05-04 20:22:07 +0100

    creating test classes

---
+++ src/tests/Dashboard.test.js
@@ -0,0 +1,9 @@
+// import '@testing-library/jest-dom'
+// import { render, screen } from '@testing-library/react';
+// import React from 'react'
+// import Dashboard from '../components/Dashboard';
+
+// test('Dashboard', () => {
+//     render(<Dashboard />);
+//     expect(screen.getByTitle(/Shopping Inspiration/i)).toMatchSnapshot();
+// })

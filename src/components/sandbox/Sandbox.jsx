1d4df809a6b264084d5d4fbcafb329627862098a
Paula63 <x17370023@student.ncirl.ie>
2021-05-06 13:59:21 +0100

    adding loading with redux thunk

--- src/components/sandbox/Sandbox.jsx
+++ src/components/sandbox/Sandbox.jsx
@@ -6,14 +6,15 @@ import { decrement, increment, } from './testReducer';
 export default function Sandbox() {
 
     const dispatch = useDispatch();
+    const {loading} = useSelector((state) => state.async)
     const data = useSelector(state => state.test.data);
 
     return (
         <>
             <h1>Testing 123</h1>
             <h3>Data is: {data}</h3>
-            <Button onClick={() => dispatch(increment(20))} style={{ backgroundColor: 'green'}}>Increment</Button>
-            <Button onClick={() => dispatch(decrement(10))} style={{ backgroundColor: 'red'}}>Decrement</Button>
+            <Button loading={loading} onClick={() => dispatch(increment(20))} style={{ backgroundColor: 'green'}}>Increment</Button>
+            <Button loading={loading} onClick={() => dispatch(decrement(10))} style={{ backgroundColor: 'red'}}>Decrement</Button>
         </>
     )
 }

27f018137db32115cb1b8cf888fa7132d68f1a53
Paula63 <x17370023@student.ncirl.ie>
2021-05-06 11:48:34 +0100

    updating delete button to trash can icon

--- src/components/wish-boards/Wishboards.jsx
+++ src/components/wish-boards/Wishboards.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import firebase from './../../firebase';
+import * as RiIcons from 'react-icons/ri';
 import { Button } from 'react-bootstrap';
+import { getWishboardsFromFirestore, dataFromSnapshot } from '../../firebase/firestoreService';
 
 
 export default function Wishboards() {
@@ -26,6 +28,14 @@ export default function Wishboards() {
         getWishboards();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    useEffect(() => {
+        const unsubscribe = getWishboardsFromFirestore({
+            next: snapshot => console.log(snapshot.docs.map(docSnapshot => dataFromSnapshot(docSnapshot))),
+            error: error => console.log(error)
+        })
+        return unsubscribe
+    })
 
     if (loading) {
         return <h1>Loading ..</h1>
@@ -58,16 +68,23 @@ export default function Wishboards() {
                     className="d-flex align-items-center justify-content-center mt-3 shadow" 
                     style={{ width: '34rem', height: '65px', borderRadius: '18px', fontSize: '18px', background: '#fff', color: '#000', borderColor: '#fff' }}
                 >
-                        <div key={wishboard.id}>
+                        <div className="flex flex-row" key={wishboard.id}>
                             <h3>{wishboard.title}</h3>
-                            <div className="w-100">
+                            {/* <div className="w-100">
                                 <Button className="w-50" onClick={() => editWishboard({ title: wishboard.title, id: wishboard.id })}>
                                     Edit
                                 </Button>
                                 <Button className="w-50" onClick={() => deleteWishboard(wishboard)}>
                                     Delete
                                 </Button>
-                            </div>
+                            </div> */}
+                        </div>
+                        <div className="ml-4" style={{ width: '30px', height: '30px'}}>
+                            <RiIcons.RiDeleteBin2Line 
+                                className="d-flex justify-content-center" 
+                                style={{ width: '100%', height: '100%', float: 'right', justifySelf: 'end'}} 
+                                onClick={() => deleteWishboard(wishboard)} 
+                            />
                         </div>
                 </Button>
             ))}

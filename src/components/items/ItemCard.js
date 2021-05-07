2cae27f441ed1c06a117210b6a213e1cdb547a2a
Paula63 <x17370023@student.ncirl.ie>
2021-05-08 00:13:35 +0100

    trying to pass item data from firestore

--- src/components/items/ItemCard.js
+++ src/components/items/ItemCard.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './container.css';
+import firebase from '../../firebase';
 
-const ItemCard = props => {
+export default function ItemCard() {
+
+    const [wishboardItem, setWishboardItem] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const ref = firebase.firestore().collection("wishboards").doc().collection('items');
+
+    function getItems() {
+        setLoading(true);
+        ref.onSnapshot((querySnapshot) => {
+            const items = [];
+            querySnapshot.forEach((doc) => {
+                items.push(doc.data());
+            })
+            setWishboardItem(items);
+            setLoading(false);
+        });
+    }
+
+    useEffect(() => {
+        getItems();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
-        <div className="item text-center shadow" style={{ width: '240px', height: '420px' }}>
-            <div className="cont cont-size">
-                <a href={props.link}>
-                    <img src={props.imgsrc} alt="item" className="item-img-top" />
-                </a>
-            </div>
-            <div className="item-title text-dark">
-                <p className="item-name" style={{ marginTop: '-40px' }}>{props.name}</p>
-                <p className="item-text text-secondary">Size: {props.size}</p>
-                <p className="item-text text-secondary">Price: {props.price}</p>
-            </div>
-        </div>
+        <>
+            {wishboardItem.map((items) => (
+                <div className="item text-center shadow" style={{ width: '240px', height: '420px' }} key={items.id}>
+                    {/* <div className="cont cont-size">
+                        <a href={wishboard.items.pathURL}>
+                            <img src={wishboard.items.photoURL} alt="item" className="item-img-top" />
+                        </a>
+                    </div> */}
+                    <div className="item-title text-dark">
+                        <p className="item-name" style={{ marginTop: '-40px' }}>{items.displayName}</p>
+                        <p className="item-text text-secondary">Size: {items.size}</p>
+                        <p className="item-text text-secondary">Price: {items.price}</p>
+                    </div>
+                </div>
+            ))}
+        </>
     )
 }
-
-export default ItemCard;

f7fd67658114c259c01256136af47d3ae314cb7c
Paula63 <x17370023@student.ncirl.ie>
2021-05-08 15:46:06 +0100

    rendering wishboard title at the top of the page; has bug that is to be fixed

--- src/components/items/Items.jsx
+++ src/components/items/Items.jsx
@@ -3,7 +3,7 @@ import firebase from './../../firebase';
 import AppBar from './../app-bar/AppBar';
 import ItemCard from './ItemCard';
 
-export default function Items({match}) {
+export default function Items() {
 
     const [wishboards, setWishboards] = useState([]);
     const [loading, setLoading] = useState(false);
@@ -27,17 +27,21 @@ export default function Items({match}) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    if (loading) {
+        return <h1>Loading ..</h1>
+    }
+
     return (
         <>
             <AppBar />
             {wishboards.map((wishboard) => (
                 <div key={wishboard.id}>
-                    <h2 className="d-flex align-items justify-content-center mt-5" style={{ fontFamily: 'Open Sans, sans-serif' }}>{wishboard.name}</h2>
+                    <h2 className="d-flex align-items justify-content-center mt-5" style={{ fontFamily: 'Open Sans, sans-serif' }}>{wishboard.title}</h2>
                 </div>
             ))}
             <div className="container-fluid d-flex justify-content-center">
-                <div className="row">
-                    <div className="col-md-4">
+                <div className="col-md-4">
+                    <div className="row">
                         <ItemCard />
                     </div>
                 </div>

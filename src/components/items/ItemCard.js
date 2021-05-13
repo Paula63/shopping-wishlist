bcd422b997c64640db27d57668a04559b148480e
Paula63 <x17370023@student.ncirl.ie>
2021-05-13 10:53:57 +0100

    updating the item data shown in cards

--- src/components/items/ItemCard.js
+++ src/components/items/ItemCard.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './container.css';
 import firebase from '../../firebase';
 import * as AiIcon from 'react-icons/ai';
+import * as FiIcons from 'react-icons/fi';
+import Shopping from '../../images/shopping.svg';
 
 export default function ItemCard() {
 
@@ -29,22 +31,23 @@ export default function ItemCard() {
     }, []);
 
     if (loading) {
-        return <h1>Loading ..</h1>
+        return <p>Loading ..</p>
     }
 
     return (
         <>
             {savedItem.map((item) => (
-                <div className="item text-center shadow" style={{ width: '240px', height: '420px', marginRight: '8px' }} key={item.id}>
-                    <div className="cont cont-size">
-                        <a href={item.location}>
-                            <img src={item.photoURL} alt="item" className="item-img-top" />
+                <div className="item text-center shadow" style={{ width: '240px', height: '380px', marginRight: '8px' }} key={item.id}>
+                    <div className="cont cont-size" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <a href={item.pathURL}>
+                            <FiIcons.FiAperture className="item-img-top" />
+                            {/* <img src={Shopping} alt="item" className="item-img-top" style={{ }} /> */}
                         </a>
                     </div>
                     <div className="item-title text-dark">
-                        <p className="item-name" style={{ marginTop: '-40px' }}>{item.displayName}</p>
-                        <p className="item-text text-secondary">Size: {item.size}</p>
-                        <p className="item-text text-secondary">Price: {item.price}</p>
+                        <p className="item-name" style={{ wordWrap: 'break-word' }}>{item.displayName}</p>
+                        {/* <p className="item-text text-secondary">Size: {item.size}</p>
+                        <p className="item-text text-secondary">Price: {item.price}</p> */}
                         <div style={{ marginTop: '-15px', marginLeft: '180px' }}>
                             <AiIcon.AiOutlineClose style={{ width: '25px', height: '25px' }} />
                         </div>

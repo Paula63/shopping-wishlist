cb00e1109091a9827b39bb1329660730608ce074
Paula63 <x17370023@student.ncirl.ie>
2021-05-15 00:17:02 +0100

    updating title size

--- src/components/wish-boards/Wishboards.jsx
+++ src/components/wish-boards/Wishboards.jsx
@@ -1,18 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import firebase from './../../firebase';
 import * as RiIcons from 'react-icons/ri';
-import * as BiIcons from 'react-icons/bi';
 import { Button } from 'react-bootstrap';
-// import { listenToWishboardsFromFirestore } from '../../firebase/firestoreService';
-import { Link } from 'react-router-dom';
-// import { listenToWishboards } from '../../actions/boardActions';
-// import { useDispatch, useSelector } from 'react-redux';
-// import useFirestoreCollection from '../../hooks/useFirestoreCollection';
 
 export default function Wishboards() {
 
-    // const dispatch = useDispatch();
-    // const {wishboards} = useSelector((state) => state.wishboard)
     const [wishboards, setWishboards] = useState([]);
     const [loading, setLoading] = useState(false);
 
@@ -35,21 +27,8 @@ export default function Wishboards() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    // useFirestoreCollection({
-    //     query: () => listenToWishboardsFromFirestore(),
-    //     data: wishboards => dispatch(listenToWishboards(wishboards)),
-    //     deps: [dispatch]
-    // });
-
-    // not needed
-    // useFirestoreDoc({
-    //     query: () => listenToWishboardFromFirestore(match.params.id),
-    //     data: wishboard => dispatchEvent(listenToWishboards([wishboard])),
-    //     deps: [match.params.id]
-    // });
-
     if (loading) {
-        return <h1>Loading ..</h1>
+        return <p>Loading ..</p>
     }
 
     // delete board
@@ -62,16 +41,6 @@ export default function Wishboards() {
             });
     }
 
-    //edit wishboard
-    // function editWishboard(updatedWishboard) {
-    //     ref
-    //         .doc(updatedWishboard.id)
-    //         .update(updatedWishboard)
-    //         .catch((err) => {
-    //             console.log(err);
-    //         });
-    // }
-
     return (
         <>
             {wishboards.map((wishboard) => (
@@ -80,18 +49,13 @@ export default function Wishboards() {
                     style={{ width: '34rem', height: '65px', borderRadius: '18px', fontSize: '18px', background: '#fff', color: '#000', borderColor: '#fff' }}
                 >
                         <div className="flex flex-row" key={wishboard.id}>
-                            <h3>{wishboard.title}</h3>
+                            <h3 style={{ fontSize: '22px' }}>{wishboard.title}</h3>
                         </div>
                         <div className="flex flex-row ml-4">
                             <RiIcons.RiDeleteBin2Line 
                                 style={{ width: '30px', height: '30px', float: 'right', justifySelf: 'end'}} 
                                 onClick={() => deleteWishboard(wishboard)} 
                             />
-                            <Link to={`/boards/${wishboard.id}`} style={{ color: '#000' }} key={wishboard.id}>
-                                <BiIcons.BiWindowOpen 
-                                    style={{ width: '30px', height: '30px', float: 'right', justifySelf: 'end', marginRight: '5px'}} 
-                                />
-                            </Link>
                         </div>
                 </Button>
             ))}

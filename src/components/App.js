1eae2ba2d8945899b14119fa4c30370259cfe3ef
Paula63 <x17370023@student.ncirl.ie>
2021-05-16 20:25:50 +0100

    making path private

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,6 @@ import Items from './items/Items';
 import ViewAll from './items/ViewAll';
 import NewItemForm from './items/NewItemForm';
 import ManageItemHome from './items/ManageItemHome';
-import Sandbox from "./sandbox/Sandbox";
 import Services from "./footer/Services";
 import HowItWorks from "./footer/HowItWorks";
 import ContactUs from "./footer/ContactUs";
@@ -36,18 +35,13 @@ function App() {
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
               <Route path="/forgot-password" component={ForgotPassword} />
-              {/* 
-                Change the route of this to PrivateRoute to ensure that the user 
-                can't access app without a valid account 
-              */}
-              <Route exact path="/" component={Dashboard} />
               <Route path="/main" component={Landing} />
-              <Route path="/sandbox" component={Sandbox} />
               <Route path="/services" component={Services} />
               <Route path="/works" component={HowItWorks} />
               <Route path="/contact" component={ContactUs} />
               <Route path="/support" component={Support} />
               <Route path="/tips" component={Tips} />
+              <PrivateRoute exact path="/" component={Dashboard} />
               <PrivateRoute exact path="/settings" component={Setting} />
               <PrivateRoute exact path="/boards" component={WishboardHome} />
               <PrivateRoute exact path="/boards/new-board" component={NewBoardForm} />
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import testReducer from '../components/sandbox/testReducer';
 import boardReducer from '../actions/boardReducer';
 import asyncReducer from './asyncReducer';
 
 const rootReducer = combineReducers({
-    test: testReducer,
     wishboard: boardReducer,
     async: asyncReducer
 })

3b3b05fcadfebc1ce94219b9167982d2e210a440
Paula63 <x17370023@student.ncirl.ie>
2021-05-16 01:55:06 +0100

    testing

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import "firebase/auth";
 
 const app = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_FIREBASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAc_5poKCkYEJ0txE86yggkqIhfeoZwVf8",
+    authDomain: "shopping-wishlist-cdd83.firebaseapp.com",
+    databaseURL: "https://shopping-wishlist-cdd83-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "shopping-wishlist-cdd83",
+    storageBucket: "shopping-wishlist-cdd83.appspot.com",
+    messagingSenderId: "789719234405",
+    appId: "1:789719234405:web:b27751ca8045782ccd138c",
+    measurementId: "G-82FJ7LGYX7"
 };
 
 firebase.initializeApp(app);
